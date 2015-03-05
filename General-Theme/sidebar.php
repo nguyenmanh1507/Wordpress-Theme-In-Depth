f897ea8456fb0497a0a674b6b51b2f907834c86f
nguyenmanh1507 <nguyenmanh1507@gmail.com>
2015-03-05 11:16:24 +0700

    Add code for sidebar.php file

---
+++ General-Theme/sidebar.php
@@ -0,0 +1,34 @@
+<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+	<div id="sidebar" class="widget-area">
+		<?php dynamic_sidebar( 'sidebar' ); ?>
+	</div>
+
+<?php else : ?>
+	<div id="sidebar" class="sidebar">
+		
+		<?php if ( has_nav_menu( 'sidebar' ) ) : ?>
+		<div class="widget nav-sidebar">
+			<h2><?php _e( 'Sidebar Menu', 'generaltheme' ); ?></h2>
+			<?php wp_nav_menu( array (
+					'theme_location'   => 'sidebar',
+					'container'        => false
+			) ); ?>
+		</div>
+	</div>
+
+<?php endif; ?>
+
+<div class="widget widget-search">
+	<h2><?php _e( 'Search', 'generaltheme' ); ?></h2>
+	<?php get_search_form(); ?>
+</div>
+
+<div class="widget widget-pages">
+	<h2><?php _e( 'Pages', 'generaltheme' ); ?></h2>
+	<ul>
+		<?php wp_list_pages( array (
+				'title_li'      => '',
+				'depth'         => 1
+		) ); ?>
+	</ul>
+</div>
